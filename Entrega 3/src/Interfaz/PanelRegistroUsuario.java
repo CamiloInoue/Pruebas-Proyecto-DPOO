7f40f2f2998434f6e6b85054de026e6249652703
CamiloInoue <123422143+CamiloInoue@users.noreply.github.com>
2023-11-13 23:35:14 -0500

    Update PanelRegistroUsuario.java

--- Entrega 3/src/Interfaz/PanelRegistroUsuario.java
+++ Entrega 3/src/Interfaz/PanelRegistroUsuario.java
@@ -1,5 +1,6 @@
 package Interfaz;
 
+import java.awt.Component;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -9,6 +10,8 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Properties;
 
 import javax.swing.ComboBoxModel;
 import javax.swing.DefaultComboBoxModel;
@@ -18,15 +21,25 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilDateModel;
+
+import modelo.DateLabelFormater;
 
 public class PanelRegistroUsuario extends JPanel implements ActionListener{
 	private VentanaPrincipal ventana;
 	private JLabel lblTitulo;
 	private JLabel lblNombre;
 	private JLabel lblApellidos;
+	private JLabel lblFechaNac;
 	private JLabel lblTelefono;
 	private JLabel lblEmail;
 	private JLabel lblId;
+	private JLabel lblIdLic;
+	private JLabel lblFechaVen;
 	private JLabel lblPais;
 	private JLabel lblTipo;
 	private JLabel lblUsuario;
@@ -38,6 +51,7 @@ public class PanelRegistroUsuario extends JPanel implements ActionListener{
 	private JTextField txtTelefono;
 	private JTextField txtEmail;
 	private JTextField txtId;
+	private JTextField txtIdLic;
 	private JTextField txtUsuario;
 	private JTextField txtContrasenia;
 
@@ -49,9 +63,11 @@ public class PanelRegistroUsuario extends JPanel implements ActionListener{
 	
 	private String rol = "";
 	
+	private Date selectedDateFecVen;
+	private Date selectedDateFecNac;
+	
 	
 	public PanelRegistroUsuario(VentanaPrincipal ventana) {
-		// TODO Auto-generated constructor stub
 		this.ventana = ventana;
 		
 		setLayout(new GridBagLayout());
@@ -65,16 +81,41 @@ public class PanelRegistroUsuario extends JPanel implements ActionListener{
 		lblTitulo = new JLabel("Ingrese informaci�n para registrar usuario");
 		lblTitulo.setFont(new Font("Tahoma", Font.BOLD, 15));
 		
-		lblNombre = new JLabel("Nombre:");
-		lblApellidos = new JLabel("Apellidos:");
-		lblTelefono = new JLabel("Telefono:");
-		lblEmail = new JLabel("Email:");
-		lblId = new JLabel("ID:");
-		lblPais = new JLabel("Pais:");
-		lblTipo = new JLabel("Tipo de usuario:");
-		lblUsuario = new JLabel("Nombre de usuario:");
-		lblContrasenia = new JLabel("Contraseña:");
-		lblFoto = new JLabel("Ingrese foto de documento:");
+		lblNombre = 		new JLabel("                          Nombre:", SwingConstants.LEFT);
+		lblApellidos = 		new JLabel("                       Apellidos:", SwingConstants.LEFT);
+		lblFechaNac = 		new JLabel("             Fecha de nacimiento:", SwingConstants.LEFT);
+		lblTelefono = 		new JLabel("                        Telefono:", SwingConstants.LEFT);
+		lblEmail = 			new JLabel("                           Email:", SwingConstants.LEFT);
+		lblId = 			new JLabel("                              ID:", SwingConstants.LEFT);
+		lblIdLic = 			new JLabel("              Número de licencia:", SwingConstants.LEFT);
+		lblFechaVen = 		new JLabel("Fecha de vencimiento de licencia:", SwingConstants.LEFT);
+		lblPais = 			new JLabel("                            Pais:", SwingConstants.LEFT);
+		lblTipo = 			new JLabel("                 Tipo de usuario:", SwingConstants.LEFT);
+		lblUsuario = 		new JLabel("               Nombre de usuario:", SwingConstants.LEFT);
+		lblContrasenia = 	new JLabel("                      Contraseña:", SwingConstants.LEFT);
+		lblFoto = 			new JLabel("       Ingrese foto de documento:", SwingConstants.LEFT);
+		
+		
+		
+		UtilDateModel model1 = new UtilDateModel();
+		Properties p1 = new Properties();
+		p1.put("text.today", "Today");
+		p1.put("text.month", "Month");
+		p1.put("text.year", "Year");
+		JDatePanelImpl datePanelFecNac = new JDatePanelImpl(model1, p1);
+		JDatePickerImpl datePickerFecNac = new JDatePickerImpl(datePanelFecNac, new DateLabelFormater());
+		selectedDateFecNac = (Date) datePickerFecNac.getModel().getValue();
+
+		
+		
+		UtilDateModel model2 = new UtilDateModel();
+		Properties p2 = new Properties();
+		p2.put("text.today", "Today");
+		p2.put("text.month", "Month");
+		p2.put("text.year", "Year");
+		JDatePanelImpl datePanelFecVen = new JDatePanelImpl(model2, p2);
+		JDatePickerImpl datePickerFecVen = new JDatePickerImpl(datePanelFecVen, new DateLabelFormater());
+		selectedDateFecVen = (Date) datePickerFecNac.getModel().getValue();
 		
 		btnRegistrar = new JButton("Registrar");
 		btnRegistrar.addActionListener(this);
@@ -89,8 +130,11 @@ public class PanelRegistroUsuario extends JPanel implements ActionListener{
 		txtTelefono = new JTextField(20);
 		txtEmail = new JTextField(40);
 		txtId = new JTextField(20);
+		txtIdLic = new JTextField(20);
 		txtUsuario = new JTextField(20);
 		txtContrasenia = new JTextField(20);
+		
+		txtNombre.setSize(150, 30);
 		
 		String[] opciones = {"Cliente", "Empleado", "Administrador Local"};
         DefaultComboBoxModel<String> modelo = new DefaultComboBoxModel<>(opciones);
@@ -140,11 +184,17 @@ public class PanelRegistroUsuario extends JPanel implements ActionListener{
 		gbc.gridy++;
 		add(lblApellidos, gbc);
 		gbc.gridy++;
+		add(lblFechaNac, gbc);
+		gbc.gridy++;
 		add(lblTelefono, gbc);
 		gbc.gridy++;
 		add(lblEmail, gbc);
 		gbc.gridy++;
 		add(lblId, gbc);
+		gbc.gridy++;
+		add(lblIdLic, gbc);
+		gbc.gridy++;
+		add(lblFechaVen, gbc);
 		gbc.gridy++;
 		add(lblPais, gbc);
 		gbc.gridy++;
@@ -165,11 +215,17 @@ public class PanelRegistroUsuario extends JPanel implements ActionListener{
 		gbc.gridy++;
 		add(txtApellidos, gbc);
 		gbc.gridy++;
+		add(datePickerFecNac, gbc);
+		gbc.gridy++;
 		add(txtTelefono, gbc);
 		gbc.gridy++;
 		add(txtEmail, gbc);
 		gbc.gridy++;
 		add(txtId, gbc);
+		gbc.gridy++;
+		add(txtIdLic, gbc);
+		gbc.gridy++;
+		add(datePickerFecVen, gbc);
 		gbc.gridy++;
 		add(comboPais, gbc);
 		gbc.gridy++;
@@ -190,6 +246,8 @@ public class PanelRegistroUsuario extends JPanel implements ActionListener{
 		add(new JLabel(), gbc);
 		gbc.gridx++;
 		
+		
+		lblApellidos.setHorizontalAlignment(SwingConstants.RIGHT);
 		
 	}
 	private void asignarRolDesdeComboBox() {
@@ -241,28 +299,30 @@ public class PanelRegistroUsuario extends JPanel implements ActionListener{
 		            }
 		        });
 				
-				
+	
 	}
 
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
 		if(e.getActionCommand().equals("Registrar")) {
+			
 			String rol = this.rol; //deben ser "cliente","administradorLocal" o "empleado"
 			System.out.print(rol);
 			String username = txtUsuario.getText();
 			String password = txtContrasenia.getText();
 			String nombre = txtNombre.getText();
+			String apellido = txtApellidos.getText();
 			String telefono = txtTelefono.getText();
 			String email = txtEmail.getText();
-			String apellido = txtApellidos.getText();
 			String pais = String.valueOf(comboPais.getSelectedItem());
-			LocalDateTime fechaNacimiento = null; //TODO
-			String idLicencia=null; //TODO
-			LocalDateTime fechaVencimientoLicencia=null;//TODO
-			BufferedImage imagenLicencia=null; //TODO
-			BufferedImage imagenDocumento = null; //TODO
-			String idDocumento = null; //TODO
+			Date fechaNacimiento = selectedDateFecNac;
+			String idDocumento = txtId.getText();
+			BufferedImage imagenDocumento = null; //La dirección de la foto queda guardada en String foto //TODO
+			String idLicencia = txtIdLic.getText();
+			BufferedImage imagenLicencia=null; //La dirección de la foto queda guardada en String licencia //TODO
+			Date fechaVencimientoLicencia = selectedDateFecVen;
 			try {
 				this.ventana.registrarUsuario(username,password, rol,nombre, telefono,email,apellido,pais,fechaNacimiento, idLicencia,fechaVencimientoLicencia,imagenLicencia,imagenDocumento,idDocumento);
 				JOptionPane.showMessageDialog(null, "Usuario creado");
@@ -274,6 +334,9 @@ public class PanelRegistroUsuario extends JPanel implements ActionListener{
 			}
 		} else if (e.getActionCommand().equals("Foto")) {
 			String foto = JOptionPane.showInputDialog("Ingrese link de la foto");
+			// Para que el usuario ingrese la dirección en la que se encuentra la imagen que debe estar en los archivos del programa
+		}else if (e.getActionCommand().equals("Licencia")) {
+			String licencia = JOptionPane.showInputDialog("Ingrese link de la foto");
 			// Para que el usuario ingrese la dirección en la que se encuentra la imagen que debe estar en los archivos del programa
 		}
 		
